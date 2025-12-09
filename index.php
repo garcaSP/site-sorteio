2325b495e776d6e804065fe22596347000a2cb4c
Thierry <thierry.quevedo@unesp.br>
2025-12-09 00:12:34 -0300

    dadasdas

--- index.php
+++ index.php
@@ -3,15 +3,20 @@
     $_SESSION['page'] = "index.php";
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="styles/styles.css">
+    <title>O tal</title>
 </head>
 <body>
-    <a href="php/criarSorteio.php">Criar O "TAL"</a>
-    <a href="destruir.php"> destruir sessao </a>
-    <a href="sessao.php"> sessao </a>
+    <main>
+        <div class='links'>
+            <a href="php/criarSorteio.php" class='otal'>Criar O "TAL"</a>
+            <a href="destruir.php"> destruir sessao </a>
+            <a href="sessao.php"> sessao </a>
+        </div>
+    </main>
 </body>
 </html>
